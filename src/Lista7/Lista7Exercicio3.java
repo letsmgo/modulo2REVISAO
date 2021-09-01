4b111cd34a9d3a81910ce4466bd820804edf419e
Let Marçal <leticiamarcalmatias@gmail.com>
2021-09-01 16:28:34 -0300

    Exercicio 3

--- src/Lista7/Lista7Exercicio3.java
+++ src/Lista7/Lista7Exercicio3.java
@@ -1,2 +1,25 @@
-package Lista7;public class Lista7Exercicio3 {
+package Lista7;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Lista7Exercicio3 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        List<Integer> tabuada = new ArrayList<>();
+
+        int valorInformado;
+
+        System.out.println("Digite o numero que você deseja conhecer a tabuada :");
+        valorInformado = leitor.nextInt();
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(valorInformado + " x " + i + " = " + (valorInformado * i));
+
+        }
+
+
+
+    }
 }
