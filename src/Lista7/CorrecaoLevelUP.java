d904a893bc00095f251d816fbf6bc32432bfeb1e
Let Marçal <leticiamarcalmatias@gmail.com>
2021-09-02 15:21:27 -0300

    Entrega 2

--- src/Lista7/CorrecaoLevelUP.java
+++ src/Lista7/CorrecaoLevelUP.java
@@ -9,15 +9,40 @@ public class CorrecaoLevelUP {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         Map<String, String> alunos = new  HashMap<String, String>();
+        boolean chave = true;
 
         System.out.println("Seja bem vindo ao Estrelas Classroom");
-        System.out.println("Digite o nome completo do aluno: ");
-        String nome = leitor.nextLine();
-        System.out.println("Digite o seu telefone: ");
-        String telefone = leitor.nextLine();
-        System.out.println("Digite seu e-mail: ");
-        String email = leitor.nextLine();
+        int opcao = 0;
+
+        while (chave == true) {
+            //Menu - apresentar uma série de opcoes pro usuario
+            System.out.println("Digite 1: Para cadastrar um aluno;");
+            System.out.println("Digite 2: Para sair do gerenciador.");
+            opcao = leitor.nextInt();
+            leitor.nextLine();
+
+            if (opcao == 1) {
+                //cadastrar um aluno
+                System.out.println("Digite o nome completo do aluno: ");
+                String nome = leitor.nextLine();
+                System.out.println("Digite o seu telefone: ");
+                String telefone = leitor.nextLine();
+                System.out.println("Digite seu e-mail: ");
+                String email = leitor.nextLine();
+
+                //fazendo o cadastro
+                alunos.put(email + " ", " Nome: " + nome + " Telefone: " + telefone);
+                System.out.println(alunos);
+
+            }else if (opcao == 2) {
+                // sair do menu
+                chave = false;
+            }else {
+                System.out.println("Digite um valor válido");
+            }
+        }
 
 
     }
+
 }
