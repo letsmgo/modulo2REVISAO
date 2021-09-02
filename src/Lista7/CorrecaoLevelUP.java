fb5035981ecd5735d3ce43226cbfcaa211f2a23a
Let Marçal <leticiamarcalmatias@gmail.com>
2021-09-02 14:57:40 -0300

    Primeira entrega

---
+++ src/Lista7/CorrecaoLevelUP.java
@@ -0,0 +1,23 @@
+package Lista7;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CorrecaoLevelUP {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        Map<String, String> alunos = new  HashMap<String, String>();
+
+        System.out.println("Seja bem vindo ao Estrelas Classroom");
+        System.out.println("Digite o nome completo do aluno: ");
+        String nome = leitor.nextLine();
+        System.out.println("Digite o seu telefone: ");
+        String telefone = leitor.nextLine();
+        System.out.println("Digite seu e-mail: ");
+        String email = leitor.nextLine();
+
+
+    }
+}
