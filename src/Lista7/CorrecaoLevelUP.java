85b0d1e106d26953205ca0773062670280597317
Let Marçal <leticiamarcalmatias@gmail.com>
2021-09-02 15:58:09 -0300

    Entrega 3

--- src/Lista7/CorrecaoLevelUP.java
+++ src/Lista7/CorrecaoLevelUP.java
@@ -8,16 +8,19 @@ import java.util.Scanner;
 public class CorrecaoLevelUP {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        Map<String, String> alunos = new  HashMap<String, String>();
+        Map<String, String> alunos = new HashMap<String, String>();
         boolean chave = true;
 
-        System.out.println("Seja bem vindo ao Estrelas Classroom");
+        System.out.println("Seja bem vindo ao Estrelas Classroom \n");
         int opcao = 0;
 
         while (chave == true) {
             //Menu - apresentar uma série de opcoes pro usuario
-            System.out.println("Digite 1: Para cadastrar um aluno;");
-            System.out.println("Digite 2: Para sair do gerenciador.");
+            System.out.println("\n Digite 1: Para cadastrar um aluno;");
+            System.out.println("Digite 2: Para visualizar os alunos do gerenciador.");
+            System.out.println("Digite 3: Para remover um aluno");
+            System.out.println("Digite 4: Para sair do gerenciador.");
+
             opcao = leitor.nextInt();
             leitor.nextLine();
 
@@ -31,13 +34,31 @@ public class CorrecaoLevelUP {
                 String email = leitor.nextLine();
 
                 //fazendo o cadastro
-                alunos.put(email + " ", " Nome: " + nome + " Telefone: " + telefone);
+                alunos.put(" " + email + " ", " Nome: " + nome + " Telefone: " + telefone);
                 System.out.println(alunos);
 
-            }else if (opcao == 2) {
+            } else if (opcao == 2) {
+                for (String chaveAluno : alunos.keySet()) {
+                    System.out.println("Contato: " + alunos.get(chaveAluno) + "Email: " + chaveAluno);
+                }
+            } else if (opcao == 3) {
+                System.out.println("Digite o e-mail a ser deletado: ");
+                String emailDeletado = "";
+                String emailASerDeletado = leitor.nextLine();
+                for (String emailAluno : alunos.keySet()) {
+                    if (emailAluno.equals(emailASerDeletado)) {
+                        System.out.println("E-mail deletado com sucesso!");
+                        emailDeletado =emailASerDeletado;
+                        // break; possibilidade para parar o loop
+                    }
+                }
+                alunos.remove(emailASerDeletado);
+            }
+            else if (opcao == 4) {
                 // sair do menu
                 chave = false;
-            }else {
+                System.out.println("Gerenciador finalizado com sucesso");
+            } else {
                 System.out.println("Digite um valor válido");
             }
         }
